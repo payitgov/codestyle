fe224ee42767aebc5db8496d3f0878925654e7fe
Matt Menzenski <matt@payitgov.com>
2021-08-02 12:00:50 -0500

    Update required pre-merge checklist and PR template to satisfy change control process obligations

--- .github/validate_pull_request.py
+++ .github/validate_pull_request.py
@@ -18,32 +18,32 @@ def is_whitespace(val: str) -> bool:
 
 
 def main(description: str):
-    q1_check_pattern = re.compile(r'1\.\s+\[(?P<check>[Xx ]?)\]\s+Is this a new service', re.MULTILINE)
+    q1_check_pattern = re.compile(r'1\.\s+\[(?P<check>[Xx ]?)\]\s+Is this a new service')
     q1_match = re.search(q1_check_pattern, description)
     if not q1_match:
         raise ValueError('Response to question "Is this a new service?" is missing')
 
-    q2_check_pattern = re.compile(r'2\.\s+\[(?P<check>[Xx ]?)\]\s+Is this change in alignment with the standard.*\\n+(?P<comment>.*)3', re.MULTILINE)
+    q2_check_pattern = re.compile(r'2\.\s+\[(?P<check>[Xx ]?)\]\s+Is this change in alignment with the standard \[back-out plan\]\(https://payitdev\.atlassian.net/wiki/spaces/SEC/pages/2833416205/Standard\+Change\+Control\+Back\+Out\+Plan\)\? If it is not, comment below this line with an alternative back-out plan\.*(\\r\\n)*(?P<comment>.*)3')
     q2_match = re.search(q2_check_pattern, description)
     if not q2_match:
         raise ValueError('Response to question "Is change in alignment with the standard back-out plan?" is missing')
 
-    q3_check_pattern = re.compile(r'3\.\s+\[(?P<check>[Xx ]?)\]\s+Will this change affect the functionality of the application', re.MULTILINE)
+    q3_check_pattern = re.compile(r'3\.\s+\[(?P<check>[Xx ]?)\]\s+Will this change affect the functionality of the application\? If it will not \(for example: if you are only updating documentation, or fixing failing tests\), you can leave the rest of this checklist alone\.')
     q3_match = re.search(q3_check_pattern, description)
     if not q3_match:
         raise ValueError('Response to question "Will this change affect the functionality of the application?" is missing')
 
-    q4_check_pattern = re.compile(r'4\.\s+\[(?P<check>[Xx ]?)\]\s+Is there a significant risk of downtime.*\\n+(?P<comment>.*)5', re.MULTILINE)
+    q4_check_pattern = re.compile(r'4\.\s+\[(?P<check>[Xx ]?)\]\s+Is there a significant risk of downtime\? If there is, comment below this line with details\.(\\r\\n)+(?P<comment>.*)5')
     q4_match = re.search(q4_check_pattern, description)
     if not q4_match:
         raise ValueError('Response to question "Is there a significant risk of downtime?" is missing')
 
-    q5_check_pattern = re.compile(r'5\.\s+\[(?P<check>[Xx ]?)\]\s+Does this change introduce new/changed dependencies to the application.*\\n+(?P<comment>.*)6', re.MULTILINE)
+    q5_check_pattern = re.compile(r'5\.\s+\[(?P<check>[Xx ]?)\]\s+Does this change introduce new/changed dependencies to the application\? If it does, comment below this line on what dependencies are added or changed\.(\\r\\n)+(?P<comment>.*)6')
     q5_match = re.search(q5_check_pattern, description)
     if not q5_match:
         raise ValueError('Response to question "Does this change introduce new/changed dependencies to the application?" is missing')
 
-    q6_check_pattern = re.compile(r'6\.\s+\[(?P<check>[Xx ]?)\]\s+Will this change affect any security controls built into the application.*\\n+(?P<comment>.*)', re.MULTILINE)
+    q6_check_pattern = re.compile(r'6\.\s+\[(?P<check>[Xx ]?)\]\s+Will this change affect any security controls built into the application\? If it does, comment below this line on what security controls are affected\.(\\r\\n)+(?P<comment>.*)')
     q6_match = re.search(q6_check_pattern, description)
     if not q6_match:
         raise ValueError('Response to question "Will this change affect any security controls built into the application?" is missing')
